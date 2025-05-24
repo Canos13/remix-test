1aec71e76a837aef570c13d01a8f5a43b5878de2
Sergio <canomsergio13@gmail.com>
2025-05-23 22:31:43 -0600

    feat: add product image

--- app/components/global/ProductImage.tsx
+++ app/components/global/ProductImage.tsx
@@ -8,27 +8,25 @@ interface ProductImage {
     clases?: string[]
 }
 
-const ProductImage = ({hasDiscount, discount, product, clases}: ProductImage) => {
-    return (
-        <div className={`product__card__item__imageContainer ${clases?.[0]}`}>
-            {
-                hasDiscount && <div className={`product__card__item__badge__discount ${clases?.[1]}`}>
-                    <Badge count={`-${discount}%`} />
-                </div>
-            }
-            <img
-                src={product?.image}
-                alt={product?.name}
-                className={`product__card__item__image ${clases?.[2]}`}
-                style={{ height: 200 }}
-                onError={e => {
-                    //@ts-ignore
-                    e.target.src =
-                        "https://st2.depositphotos.com/1001335/11384/v/450/depositphotos_113845402-stock-illustration-medicine-bottle-on-white-background.jpg";
-                }}
-            />
-        </div>
-    )
-}
+const ProductImage = ({hasDiscount, discount, product, clases}: ProductImage) =>
+    
+    <div className={`product__card__item__imageContainer ${clases?.[0]}`}>
+        {
+            hasDiscount && <div className={`product__card__item__badge__discount ${clases?.[1]}`}>
+                <Badge count={`-${discount}%`} />
+            </div>
+        }
+        <img
+            src={product?.image}
+            alt={product?.name}
+            className={`product__card__item__image ${clases?.[2]}`}
+            style={{ height: 200 }}
+            onError={e => {
+                //@ts-ignore
+                e.target.src =
+                    "https://st2.depositphotos.com/1001335/11384/v/450/depositphotos_113845402-stock-illustration-medicine-bottle-on-white-background.jpg";
+            }}
+        />
+    </div>
 
 export default ProductImage
