71537e79c1f69027a54d5f0e5b432d32f903d178
Sergio <canomsergio13@gmail.com>
2025-05-23 22:07:02 -0600

    feat: add modal

--- app/components/Product/index.tsx
+++ app/components/Product/index.tsx
@@ -1,13 +1,23 @@
 
-import { Badge, Tag } from 'antd';
+import { Modal, Tag } from 'antd';
 import { Product } from '~/context/productStore';
 import { useCartStore } from '../../context/cartStore';
 import { showMessage } from '~/utils/messages';
 import { useProductDiscount } from '~/hook/useProductDiscount';
+import { useState } from 'react';
+import ProductImage from '../global/ProductImage';
+import ProductStock from '../global/ProductStock';
 
 const ProductCard = ({ product }: { product: Product, }) => {
     const addToCart = useCartStore(state => state.addToCart);
-    const {  hasDiscount,  discount, } = useProductDiscount(product);
+    const { hasDiscount, discount, } = useProductDiscount(product);
+
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    const showModal = () => setIsModalOpen(true);
+    const handleOk = () => setIsModalOpen(false);
+    const handleCancel = () => setIsModalOpen(false);
+
 
     const handleAddToCart = (product: Product) => {
         if (!product.availability) return;
@@ -19,55 +29,49 @@ const ProductCard = ({ product }: { product: Product, }) => {
     };
 
     return (
-        <div className="product__card__item">
-            <div className='product__card__item__imageContainer'>
-                {
-                    hasDiscount && <div className='product__card__item__badge__discount'>
-                        <Badge count={`-${discount}%`} />
-                    </div>
-                }
-                <img
-                    src={product?.image}
-                    alt={product?.name}
-                    className="w-full product__card__item__image"
-                    style={{ height: 200 }}
-                    onError={e => {
-                        //@ts-ignore
-                        e.target.src =
-                            "https://st2.depositphotos.com/1001335/11384/v/450/depositphotos_113845402-stock-illustration-medicine-bottle-on-white-background.jpg";
-                    }}
+        <>
+            <div className="product__card__item" onClick={showModal}>
+                <ProductImage 
+                    discount={discount}
+                    product={product}
+                    hasDiscount={hasDiscount}
+                    clases={["","","w-full"]}
                 />
-            </div>
-            <div className="product__card__item__info">
-                <h3 className="product__card__item__info__productName">{product?.name}</h3>
+                <div className="product__card__item__info">
+                    <h3 className="product__card__item__info__productName">{product?.name}</h3>
+                    <ProductStock stock={product.stock} /> 
 
-                {
-                    product?.stock
-                        ? <Tag className="product__card__item__info__stock" color="success"> Stock: {product?.stock}</Tag  >
-                        : <Tag className="product__card__item__info__stock" color="error"> Sin Stock</Tag>
+                    <div className='product__container__prices'>
+                        <span className={`font-bold product__price ${hasDiscount ? "product__price__hasDiscount" : ""}`}>${product?.price}</span>
+                        {hasDiscount && <span className="product__listPrice">${product?.listPrice}</span>}
+                    </div>
+                    <button
+                        onClick={(e) => {
+                            e.stopPropagation();
+                            handleAddToCart(product)
+                        }}
+                        className={
+                            `px-4 py-2 text-white button__add__to__cart
+                            ${product?.availability
+                                ? "product__item__availability"
+                                : "product__item__not__availability"
+                            }`
+                        }
+                    >
+                        {product?.availability ? "Agregar" : "No disponible"}
+                    </button>
 
-                }
-
-
-                <div className='product__container__prices'>
-                    <span className={`font-bold product__price ${hasDiscount ? "product__price__hasDiscount" : ""}`}>${product?.price}</span>
-                    {hasDiscount && <span className="product__listPrice">${product?.listPrice}</span>}
                 </div>
-                <button
-                    onClick={() => handleAddToCart(product)}
-                    className={
-                        `px-4 py-2 text-white button__add__to__cart
-                        ${product?.availability
-                            ? "product__item__availability"
-                            : "product__item__not__availability"
-                        }`
-                    }
-                >
-                    {product?.availability ? "Agregar" : "No disponible"}
-                </button>
-
             </div>
-        </div>
+
+            <Modal destroyOnClose title={product?.name} onCancel={handleCancel} open={isModalOpen} className='modal__product__item' okText="Cerrar" onOk={handleOk}>
+                <ProductImage 
+                    discount={discount}
+                    product={product}
+                    hasDiscount={hasDiscount}
+                />
+            </Modal>
+        </>
     );
 };
 
