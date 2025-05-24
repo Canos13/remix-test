559a0dab6fc5f22307097a4e39588c0254ce4bc8
Sergio <canomsergio13@gmail.com>
2025-05-23 21:14:19 -0600

    feat: carrousel products responsive

--- app/routes/_index/route.tsx
+++ app/routes/_index/route.tsx
@@ -73,7 +73,23 @@ export default function Index() {
 		arrows: true,
 		dots: true,
 		slidesToShow: 4,
-		slidesToScroll: 4
+		slidesToScroll: 4,
+		responsive: [
+			{
+				breakpoint: 1024,
+				settings: {
+					slidesToShow: 3,
+					slidesToScroll: 3,
+				}
+			},
+			{
+				breakpoint: 640,
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 2,
+				}
+			}
+		]
 	}
 
 	useEffect(() => {
@@ -81,7 +97,7 @@ export default function Index() {
 			clearOrderForm()
 		}
 	}, [isAuthenticated])
-	
+
 
 	return (
 		<div className="shop__container">
